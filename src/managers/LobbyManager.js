bc61b770d84efe736a2a273ed7fc2688a8e91edd
prxgr4mm3r <berezan.ab@knu.ua>
2025-10-02 10:07:09 +0000

    unpack payload on pablishing to lobby channel

--- src/managers/LobbyManager.js
+++ src/managers/LobbyManager.js
@@ -783,7 +783,7 @@ class LobbyManager {
   broadcastToLobby(lobbyCode, event, payload) {
     this.channelManager.publish(`lobby_${lobbyCode}`, {
       action: event,
-      payload
+      ...payload
     });
   }
 
